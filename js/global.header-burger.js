838231afe3ef7bb3bdb64a9d5c3c8688ee04c449
“Maksym <todimaks1@gmail.com>
2026-02-18 12:25:13 +0200

    add header hero

--- js/global.header-burger.js
+++ js/global.header-burger.js
@@ -1,29 +1,21 @@
-// Get DOM elements
-const burger = document.querySelector(".header-nav__menu-button");
+const burger = document.querySelector(".header-nav__menu-button"); /*записуємо в змінні DOM‑елементи:*/
 const nav = document.querySelector(".header-nav__menu");
-const menuLinks = document.querySelectorAll(".header-nav__menu-link");
-// const header = document.querySelector(".header");
+const menuLinks = document.querySelectorAll(".header-nav__menu-link"); /*Шукає всі елементи з класом .header-nav__menu-link і створює зі списку масив*/
 
-// Toggle menu function
 function toggleMenu() {
   burger.classList.toggle("is-active");
   nav.classList.toggle("is-open");
-  document.body.classList.toggle("no-scroll");
-//   header.classList.toggle("no-bg");
+  document.body.classList.toggle("no-scroll"); 
 }
 
-// Close menu function
 function closeMenu() {
   burger.classList.remove("is-active");
   nav.classList.remove("is-open");
   document.body.classList.remove("no-scroll");
-//   header.classList.remove("no-bg");
 }
 
-// Add click event listener to burger button
 burger.addEventListener("click", toggleMenu);
 
-// Add click event listeners to all menu links
-menuLinks.forEach((link) => {
+menuLinks.forEach((link) => { /*Коли користувач клікає на будь-яке посилання, викликається closeMenu, і меню автоматично зникає. */
   link.addEventListener("click", closeMenu);
 });
